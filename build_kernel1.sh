cb848ee2960eaab85298032d32eb2345603a65a8
Maverick Leroux <78061003+geekmaster21@users.noreply.github.com>
2022-06-10 21:48:03 -0400

    Update build_kernel1.sh

--- build_kernel1.sh
+++ build_kernel1.sh
@@ -1,43 +1,95 @@
+#!/bin/bash
+
+# Check if have toolchain/llvm folder
+
+if [ ! -d "$(pwd)/gcc/" ]; then
+
+   git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 gcc -b android-9.0.0_r59 --depth 1 >> /dev/null 2> /dev/null
+
+fi
+
+if [ ! -d "$(pwd)/llvm-sdclang/" ]; then
+
+   git clone https://github.com/proprietary-stuff/llvm-arm-toolchain-ship-10.0 llvm-sdclang --depth 1 >> /dev/null 2> /dev/null
+
+fi
+
 # Export KBUILD flags
-export KBUILD_BUILD_USER="geekmaster21"
-export KBUILD_BUILD_HOST="geekmaster21"
+
+export KBUILD_BUILD_USER="Geekmaster21"
+
+export KBUILD_BUILD_HOST="OVH"
 
 # Export ARCH/SUBARCH flags
+
 export ARCH="arm64"
-export SUBARCH="arm64"
+
+export SUBARCH="arm"
 
 # Export ANDROID VERSION
+
 export PLATFORM_VERSION=12
-export ANDROID_MAJOR_VERSION=r
+
+export ANDROID_MAJOR_VERSION=S
 
 # Export CCACHE
+
 export CCACHE_EXEC="$(which ccache)"
+
 export CCACHE="${CCACHE_EXEC}"
+
 export CCACHE_COMPRESS="1"
+
 export USE_CCACHE="1"
+
 ccache -M 50G
 
 # Export toolchain/clang/llvm flags
+
 export CROSS_COMPILE="$(pwd)/gcc/bin/aarch64-linux-android-"
+
 export CLANG_TRIPLE="aarch64-linux-gnu-"
-export CC="$(pwd)/toolchain/clang/bin/clang"
-KERNEL_MAKE_ENV="DTC_EXT=$(pwd)/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y"
+
+export CC="$(pwd)/llvm-sdclang/bin/clang"
+
 # Export if/else outdir var
+
 export WITH_OUTDIR=true
 
 # Clear the console
+
 clear
 
 # Remove out dir folder and clean the source
-# Build time
+
 if [ "${WITH_OUTDIR}" == true ]; then
+
    if [ ! -d "$(pwd)/a71" ]; then
+
       mkdir a71
+
    fi
+
+fi
+
+# Build time
+
+if [ "${WITH_OUTDIR}" == true ]; then
+
+   if [ ! -d "$(pwd)/a71" ]; then
+
+      mkdir a71
+
+   fi
+
 fi
 
 if [ "${WITH_OUTDIR}" == true ]; then
+
    "${CCACHE}" make O=a71 a71_eur_open_defconfig
-   "${CCACHE}" make -j8 $KERNEL_MAKE_ENV O=a71
+
+   "${CCACHE}" make -j$(nproc --all) O=a71
+
    tools/mkdtimg create a71/arch/arm64/boot/dtbo.img --page_size=4096 $(find a71/arch -name "*.dtbo")
+
 fi
